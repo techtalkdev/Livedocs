c94faccaca390c09b68dcd85b775b7092cbdc8c5
Mike👨‍💻 <binarybardcode@gmail.com>
2024-08-27 15:45:04 +0200

    implement clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import React from "react"
 import { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -17,7 +19,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }:{ children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider 
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px"}
+      }}  
+    >
+      <html lang="en" suppressHydrationWarning>
             <body
         className={cn(
           "min-h-screen font-sans antialiased",
@@ -27,5 +35,6 @@ export default function RootLayout({ children }:{ children: React.ReactNode }) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
   )
 }
