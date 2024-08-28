eaa71985a2fb032510d20ceaf0959ca44a0484d0
Mike👨‍💻 <binarybardcode@gmail.com>
2024-08-28 16:21:32 +0200

    implement edit document title feature

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -50,3 +50,20 @@ export const getDocument = async ({ roomId, userId }: { roomId: string; userId: 
         
     }
 }   
+
+export const updateDocument = async ( roomId: string, title: string ) => {
+    try {
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            metadata: {
+                title,
+            }
+        })
+
+        revalidatePath(`/documents/${roomId}`); 
+
+        return parseStringify(updatedRoom); 
+
+    } catch (error) {
+        console.log(`Error happened while updating a room: ${error}`); 
+    }
+}  
